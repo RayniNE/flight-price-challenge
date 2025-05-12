3e0d82bb98ec48132adb218a4731cdcdd767e594
Rayni Nunez Espino <timetopwm@gmail.com>
2025-05-11 22:31:43 -0400

    Created the user return struct.

--- models/flights.go
+++ models/flights.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type GetFlightsDTO struct {
 	DepartDate  string `json:"depart_date,omitempty"`
 	Origin      string `json:"origin,omitempty"`
@@ -7,11 +9,17 @@ type GetFlightsDTO struct {
 }
 
 type Flights struct {
-	Price           float64 `json:"price"`
-	OriginCode      string  `json:"origin_code"`
-	OriginName      string  `json:"origin_name"`
-	DestinationCode string  `json:"destination_code"`
-	DestinationName string  `json:"destination_name"`
-	DepartureTime   string  `json:"departure_time"`
-	ArrivalTime     string  `json:"arrival_time"`
+	Price           float64   `json:"price"`
+	OriginCode      string    `json:"origin_code"`
+	OriginName      string    `json:"origin_name"`
+	DestinationCode string    `json:"destination_code"`
+	DestinationName string    `json:"destination_name"`
+	DepartureTime   time.Time `json:"departure_time,omitempty"`
+	ArrivalTime     time.Time `json:"arrival_time,omitempty"`
+}
+
+type FlightsResponse struct {
+	CheapestFlights []Flights `json:"cheapest_flights"`
+	FastestFlights  []Flights `json:"fastest_flights"`
+	OtherFlights    []Flights `json:"other_flights"`
 }
